ffcbc0edcac5b0fb48ebdbf9cae031c7d3a36571
Jiingz <49228523+Jiingz@users.noreply.github.com>
2022-02-08 23:30:55 +0100

    added cargo

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main()
     let args: Vec<String> = env::args().collect(); // target, key
     let result = xor::encrypt(&args[1], &args[2]);
 
-    fs::write("key.xor", result.clone());
+    fs::write("key.xor", result.clone()).unwrap();
 
     println!("{}", result);
 }
